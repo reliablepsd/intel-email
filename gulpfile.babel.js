bdc262c35cab70d2cac89b52866d11ba966f11c8
sharnirio-work <vyacheslav@heyreliable.com>
2023-06-27 15:29:59 +0300

    Fix for outlook

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -134,10 +134,10 @@ function inliner(css) {
     })
     .pipe($.replace, '<!-- <style> -->', `<style>${mqCss}</style>`)
     .pipe($.replace, '<link rel="stylesheet" type="text/css" href="css/app.css">', '')
-    .pipe($.htmlmin, {
-      collapseWhitespace: true,
-      minifyCSS: true
-    });
+    // .pipe($.htmlmin, {
+    //   collapseWhitespace: true,
+    //   minifyCSS: true
+    // });
 
   return pipe();
 }
